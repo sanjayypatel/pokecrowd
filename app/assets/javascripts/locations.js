00f87d6d46726a8ed5114ffe2f3a1f4ca5ff7a15
Sanjay Patel <sanjay.y.patel@gmail.com>
2016-07-12 10:39:37 -0700

    Configured MapboxGL to load map and display location

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -5,8 +5,50 @@ $(document).ready(function(){
     var saveButton = document.getElementById('save-button');
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(function(position) {
-        latField.value = position.coords.latitude;
-        lngField.value = position.coords.longitude;
+        var lat = position.coords.latitude;
+        var lng = position.coords.longitude;
+        latField.value = lat;
+        lngField.value = lng;
+        
+        // Mapbox setup
+        mapboxgl.accessToken = 'pk.eyJ1Ijoic2FuamF5eXBhdGVsIiwiYSI6ImNpbHV4OHZ5ajAxaHl1ZGtzZ3BvZmJvOWgifQ.TACQQlF02mqCdzUWUKFy4g';
+        var map = new mapboxgl.Map({
+          container: 'your-location',
+          style: 'mapbox://styles/sanjayypatel/ciqjnn3h80014bnncnkwv0i3a',
+          center: [ lng, lat ],
+          zoom: 16,
+          interactive: false
+        });
+
+        // Add a red circle at current location
+        map.on('load', function() {
+          map.addSource("points", {
+          "type": "geojson",
+            "data": {
+              "type": "FeatureCollection",
+              "features": [{
+                  "type": "Feature",
+                  "geometry": {
+                      "type": "Point",
+                      "coordinates": [ lng, lat ]
+                  }
+              }]
+            }
+          });
+          map.addLayer({
+              "id": "points",
+              "type": "circle",
+              "source": "points",
+              "paint": {
+                "circle-radius": 8,
+                "circle-color": "#db004c"
+
+              }
+          });
+        });
+
+
+        // enable save button after location load.
         saveButton.removeAttribute('disabled');
       });
     } else {
@@ -14,3 +56,5 @@ $(document).ready(function(){
     }
   }
 });
+
+
